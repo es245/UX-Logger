eb6d1c1bb04285a78bb26de97cc86ef9b8674f6f
emilsadikoski <emilenetesakat@hotmail.com>
2017-06-23 00:47:55 -0400

    Add form submission handler in Angular

--- js/main_controller.js
+++ js/main_controller.js
@@ -1,18 +1,27 @@
-app.controller('main_controller', ['$scope', function($scope) {
+app.controller('main_controller', ['$scope', '$filter', function($scope, $filter) {
 
   angular.element(document).ready(function () {
-
+    $scope.time_formats = [
+      {name:'Day/Month/Year/Hour/Min/Sec', format:'dd MMM yyyy H:mm:ss'},
+      {name:'Hour/Min/Sec/Day/Month/Year', format:'H:mm:ss dd MMM yyyy'},
+      {name:'Hour/Minute/Second', format:'H:mm:ss'},
+      {name:'Day/Month/Year', format:'dd MMM yyyy'}
+    ];
   	$scope.entry = '';
     $scope.results = '';
     $scope.list = [];
+    $scope.time_date_format = null;
+
 
     $scope.submitEntry = function(){
+      $scope.date = new Date();
+      current_time = $filter('date')($scope.date, $scope.time_date_format.format);
       if($scope.entry){
         $scope.list.push(this.entry);
-        //alert($scope.results);
-        $scope.results += $scope.entry + "\n";
+        $scope.results += current_time + ': ' + $scope.entry + "\n";
         $scope.entry = '';
       }
+
     }
 
   });
